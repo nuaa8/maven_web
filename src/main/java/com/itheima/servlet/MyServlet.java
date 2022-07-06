2e0801aaf785d6ce92531df14d1fe61dc88ef1ba
dashuai <297035528@qq.com>
2022-07-06 15:51:03 +0800

    second commit

--- src/main/java/com/itheima/servlet/MyServlet.java
+++ src/main/java/com/itheima/servlet/MyServlet.java
@@ -3,5 +3,6 @@ package com.itheima.servlet;
 public class MyServlet {
     public static void main(String[] args) {
         System.out.println("Hello,git");
+        System.out.println("Hello,git2");
     }
 }
